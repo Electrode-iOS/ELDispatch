7d0e2dce8dd252f500111c2c37dd0a4857bf249a
Brandon Sneed <brandon@redf.net>
2015-02-10 10:37:39 -0800

    Initial Commit

---
+++ THGDispatch/THGDispatch.h
@@ -0,0 +1,19 @@
+//
+//  THGDispatch.h
+//  THGDispatch
+//
+//  Created by Brandon Sneed on 2/10/15.
+//  Copyright (c) 2015 SetDirection. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for THGDispatch.
+FOUNDATION_EXPORT double THGDispatchVersionNumber;
+
+//! Project version string for THGDispatch.
+FOUNDATION_EXPORT const unsigned char THGDispatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <THGDispatch/PublicHeader.h>
+
+
