f5128c73a2504f10d9ae0485e083bcac2eb8cbe3
Brandon Sneed <brandon@redf.net>
2015-02-19 04:57:50 -0800

    Finalized copyright, license, etc.

--- THGDispatch/DispatchClosure.swift
+++ THGDispatch/DispatchClosure.swift
@@ -3,7 +3,7 @@
 //  THGDispatch
 //
 //  Created by Brandon Sneed on 2/15/15.
-//  Copyright (c) 2015 Walmart. All rights reserved.
+//  Copyright (c) 2015 TheHolyGrail. All rights reserved.
 //
 
 import Foundation
