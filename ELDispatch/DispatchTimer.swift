9dbcd09a1678c34a9014dce3079ca35efb95c22c
Angelo Di Paolo <angelod101@gmail.com>
2016-08-31 12:22:04 -0400

    update `DispatchTimer` to use dispatch_source_create iOS 10 API

--- ELDispatch/DispatchTimer.swift
+++ ELDispatch/DispatchTimer.swift
@@ -12,31 +12,45 @@ import Foundation
 import ELFoundation
 #endif
 
-/**
-Represents a GCD timer.
-*/
+/// Represents a GCD timer.
 public class DispatchTimer {
-    
-    /**
-    Initializes a new dispatch timer.
-    */
+    /// Initializes a new dispatch timer.
     public init () {
         // nothing to do here.  yet.
     }
     
+    /// The state of the timer.  `true` means the timer is suspended.  `false` means that it isn't.
+    public var suspended: Bool {
+        get {
+            return lock.around {
+                return self.isSuspended
+            }
+        }
+        
+        set(value) {
+            lock.around {
+                self.isSuspended = value
+            }
+        }
+    }
+    
+    private var rawTimer: dispatch_source_t? = nil
+    private let lock: Spinlock = Spinlock()
+    private var isSuspended: Bool = false
+    
     /**
-    Schedules a GCD timer on the specified queue.
-
-    - parameter queue: The target queue.
-    - parameter interval: The interval at which to execute the closure, in seconds.
-    - parameter delay: The time to wait before the timer beings, in seconds.  The default is 0.
-    - parameter leeway: A hint as to the leeway by which GCD may have to execute the closure.  The default is 0.2.
-    - parameter suspended: The timer will be created in a suspended state.
-    - parameter closure: The closure to execute.
+     Schedules a GCD timer on the specified queue.
+     
+     - parameter queue: The target queue.
+     - parameter interval: The interval at which to execute the closure, in seconds.
+     - parameter delay: The time to wait before the timer beings, in seconds.  The default is 0.
+     - parameter leeway: A hint as to the leeway by which GCD may have to execute the closure.  The default is 0.2.
+     - parameter suspended: The timer will be created in a suspended state.
+     - parameter closure: The closure to execute.
     */
     public func schedule(queue: DispatchQueue, interval: NSTimeInterval, delay: NSTimeInterval = 0, leeway: NSTimeInterval = 1, suspended: Bool = false, _ closure: () -> Void) -> DispatchClosure {
         // if we have a timer object already, lets bolt.
-        if rawTimer != nil {
+        if self.rawTimer != nil {
             exceptionFailure("A timer has already been scheduled!")
         }
         
@@ -44,13 +58,8 @@ public class DispatchTimer {
         // it'll get set to the proper state after.
         isSuspended = suspended
         
-        guard let rawTimer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue.dispatchQueue()) else {
-            exceptionFailure("The timer dispatch source was unable to be created.")
-            return DispatchClosure {
-                // do nothing.
-            }
-        }
         
+        let rawTimer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue.dispatchQueue())
         self.rawTimer = rawTimer
         
         var startTime: dispatch_time_t
@@ -75,7 +84,7 @@ public class DispatchTimer {
     }
     
     /**
-    Cancels the timer.
+     Cancels the timer.
     */
     public func cancel() {
         if let timer = rawTimer {
@@ -85,7 +94,7 @@ public class DispatchTimer {
     }
     
     /**
-    Suspends execution of the timer.  Call `resume()` to begin again.
+     Suspends execution of the timer.  Call `resume()` to begin again.
     */
     public func suspend() {
         if let timer = rawTimer {
@@ -99,7 +108,7 @@ public class DispatchTimer {
     }
     
     /**
-    Resumes a timer.  Call `suspend()` to suspend it.
+     Resumes a timer.  Call `suspend()` to suspend it.
     */
     public func resume() {
         if let timer = rawTimer {
@@ -111,25 +120,4 @@ public class DispatchTimer {
             // throw an error?
         }
     }
-    
-    /**
-    - returns: The state of the timer.  True means the timer is suspended.  False means that it isn't.
-    */
-    public var suspended: Bool {
-        get {
-            return lock.around {
-                return self.isSuspended
-            }
-        }
-        
-        set(value) {
-            lock.around {
-                self.isSuspended = value
-            }
-        }
-    }
-    
-    private var rawTimer: dispatch_source_t? = nil
-    private let lock: Spinlock = Spinlock()
-    private var isSuspended: Bool = false
 }
